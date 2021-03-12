06cb146ca354521145cd7b4577ce44ef39273ee4
faisaal99 <faisal.163@hotmail.com>
2021-03-12 20:23:27 +0100

    add logic to placing a piece
    the functionality to add a piece to a specific column is added in code. right now it doesn't do it gameplay-vise. It can also prevent a player from adding one if the column if full

--- hud/play_menu_hud.gd
+++ hud/play_menu_hud.gd
@@ -2,13 +2,21 @@ extends Control
 
 # Children
 onready var play_button = $CenterContainer/VBoxContainer/PlayButton
+onready var highlight = $CenterContainer/VBoxContainer/PlayButton/Highlight
 
 # Props
 signal signal_start_game
 
-
-
-
 func _on_PlayButton_pressed():
 	emit_signal("signal_start_game")
 	visible = false
+
+
+func _on_play_button_hover():
+	print("Hover")
+	highlight.visible = true
+
+
+func _on_play_button_exit_hover():
+	print("Hover Exit")
+	highlight.visible = false
--- scenes/play.gd
+++ scenes/play.gd
@@ -1,7 +1,34 @@
 extends Node2D
 
-
+# Keep track of all possible positions in the grid
+# positions[columns][rows]
+const positions = [
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0]
+]
 
 func _on_column_pressed(event, which_button):
 	if Input.is_action_just_pressed("click"):
-		print("#{nr} pressed".format({"nr": which_button}))
+		# Make sure the player is able to put a
+		# piece in the selected column
+		if positions[which_button][0] == 0:
+			_add_disc_in_column(which_button)
+		else:
+			print("The column is filled")
+
+func _add_disc_in_column(nr: int):
+	
+	var max_index = float(positions[nr].size() - 1)
+	# Loop through the entire column from end to beginning (index 5 to 0)
+	# If the current position has a '1', go to the next one until there is a '0'
+	for col in range(max_index, -1, -1):
+		if positions[nr][col] == 0:
+			positions[nr][col] = 1
+			break
+	
+	print(positions[nr])
