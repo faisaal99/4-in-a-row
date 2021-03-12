de33b8911dbc4e578869d925c188206132b8c232
faisaal99 <faisal.163@hotmail.com>
2021-03-12 21:37:00 +0100

    The user can press on a column and see a new disc appear
    The logic to add a disc is implemented. But it doesn't animate yet.

---
+++ components/disc.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+class_name Disc
+
+# Textures
+const red = preload("res://assets/textures/red.png")
+const yellow = preload("res://assets/textures/yellow.png")
+
+# Children
+onready var img = $Img
+onready var animation_player = $AnimationPlayer
+
+var color: int
+
+func _ready():
+	if color == Constants.RED:
+		img.texture = red
+	elif color == Constants.YELLOW:
+		img.texture = yellow
+	else:
+		push_error("The 'color' input is incorrect, should use either Constants.RED or Constants.YELLOW")
+	print("Instanced")
--- scenes/play.gd
+++ scenes/play.gd
@@ -12,7 +12,11 @@ const positions = [
 	[0, 0, 0, 0, 0, 0]
 ]
 
-func _on_column_pressed(event, which_button):
+# Props
+const disc = preload("res://components/disc.tscn")
+export(Array) var column_positions
+
+func _on_column_pressed(_event, which_button):
 	if Input.is_action_just_pressed("click"):
 		# Make sure the player is able to put a
 		# piece in the selected column
@@ -21,14 +25,24 @@ func _on_column_pressed(event, which_button):
 		else:
 			print("The column is filled")
 
+# Add a disc at the specified column
 func _add_disc_in_column(nr: int):
-	
 	var max_index = float(positions[nr].size() - 1)
 	# Loop through the entire column from end to beginning (index 5 to 0)
 	# If the current position has a '1', go to the next one until there is a '0'
 	for row in range(max_index, -1, -1):
 		if positions[nr][row] == 0:
 			positions[nr][row] = 1
+			_spawn_disc(nr, row)
 			break
 	
 	print(positions[nr])
+
+func _spawn_disc(col: int, row: int):
+	var disc_scene = disc.instance()
+	var position_col = get_node(column_positions[col])
+	
+	disc_scene.color = Constants.RED
+	disc_scene.position = position_col.position
+	
+	add_child(disc_scene)
