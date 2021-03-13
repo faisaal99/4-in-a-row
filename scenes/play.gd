daffc2b5d94d5b75237c8340acb8f96115589324
faisaal99 <faisal.163@hotmail.com>
2021-03-13 01:21:35 +0100

    Fall anim change and click prevention
    The fall animation is improved to make it smoother, and the user is prevented from adding a new disc before the animation has finished.

--- scenes/play.gd
+++ scenes/play.gd
@@ -22,15 +22,17 @@ const disc = preload("res://components/disc.tscn")
 const circle_and_gap_length = 36
 export(Array) var column_position_refs
 var _disc_scene: Node2D
+var _can_click = true
 
 func _on_column_pressed(_event, which_button):
-	if Input.is_action_just_pressed("click"):
+	if Input.is_action_just_pressed("click") and _can_click:
 		# Make sure the player is able to put a
 		# piece in the selected column
 		if positions[which_button][0] == 0:
+			_can_click = false
 			_add_disc_in_column(which_button)
 		else:
-			print("The column is filled")
+			pass # If the column is full
 
 # Add a disc at the specified column
 func _add_disc_in_column(nr: int):
@@ -59,6 +61,9 @@ func _spawn_disc(col: int, row: int):
 	disc_scene.position = initial_pos
 	add_child(disc_scene)
 	
-	tween.interpolate_property(disc_scene, "position", initial_pos, end_pos, 0.5, Tween.TRANS_CUBIC, Tween.EASE_IN)
+	tween.interpolate_property(disc_scene, "position", initial_pos, end_pos, 0.5, Tween.TRANS_QUAD, Tween.EASE_IN)
 	tween.start()
-	
+
+# Called when the fall animation stops
+func _on_tween_completed(object, key):
+	_can_click = true
